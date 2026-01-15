ae2a16da49b9d813d63d7a3a07bf9246dbb67fdf
rahheba <lunaayyes@gmail.com>
2026-01-15 15:17:09 +0530

    bugs fixing from build apk

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quietly/features/admin/admin_dashboard.dart';
-import 'package:quietly/features/admin/home/home_screen.dart';
 import 'package:quietly/features/auth/view/login_screen.dart';
 import 'package:quietly/features/bottom_nav/bottom_nav.dart';
 import 'package:quietly/features/parent/parent_home.dart';
